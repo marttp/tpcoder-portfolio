36858d17f40dbbd18b8ec48b1794433e6659653a
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-04-23 20:59:12 +0900

    Update 404 pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z
 			.string()
 			.or(z.date())
-			.transform((val) => new Date(val)),
+			.transform((val) => new Date(val))
+			.optional(),
 		updatedDate: z
 			.string()
 			.optional()
