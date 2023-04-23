eecc7da97c91194512ccef56e6cef901cd6f0f1f
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-04-23 21:14:11 +0900

    Adding more style on hover

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -4,7 +4,7 @@ import { Social } from '../constants/social';
 const SocialIcons = (props: Social) => {
   return (
     <a href={props.url}>
-      <div class="inline-block relative rounded-full h-8 w-8 dark:bg-primary-light m-3">
+      <div class="inline-block relative rounded-full h-8 w-8 dark:bg-primary-light m-3 hover:animate-bounce">
         <div class="absolute inset-0 rounded-full" />
         <div class="absolute inset-0 flex items-center justify-center">
           <div innerHTML={props.icon} class="h-5 w-5" />
