3d49576ca54398a357355e8a238d87b66b648c83
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-04-29 20:05:45 +0900

    Split components for competition

---
+++ src/components/competition/CompetitionItem.tsx
@@ -0,0 +1,65 @@
+import { JSX } from 'solid-js';
+import { Competition } from '../../data/props';
+
+type CompetitionProps = {
+  competition: Competition;
+  index: number;
+};
+
+const CompetitionItem = (props: CompetitionProps): JSX.Element => {
+  const { competition, index } = props;
+
+  const isImage = competition.resources?.[0]?.type === 'images';
+  const isOdd = index % 2 !== 0;
+
+  const Content = (
+    <div class={`w-full md:w-3/5 ${isImage ? 'text-center' : ''}`}>
+      <h3 class="text-xl font-bold">{competition.name}</h3>
+      <p class="text-gray-700">
+        {competition.date} - {competition.country}
+      </p>
+      {competition.detail?.award && (
+        <div>
+          <p class="text-gray-700">Award: {competition.detail?.award}</p>
+          {!isImage && (
+            <a
+              href={competition.resources?.[0]?.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              class="border-b hover:opacity-80"
+            >
+              Interesting Link
+            </a>
+          )}
+        </div>
+      )}
+    </div>
+  );
+
+  const ImageDispaly = (
+    <>
+      <div class="w-full md:w-2/5">
+        <img
+          src={competition.resources?.[0]?.url}
+          alt={competition.name}
+          class={`${
+            isOdd ? 'ml-4' : 'mr-4'
+          } rounded-lg object-cover object-center w-full h-full md:h-60 mb-4 md:mb-0`}
+        />
+      </div>
+      {Content}
+    </>
+  );
+
+  return (
+    <div
+      class={`${
+        isImage && isOdd ? 'flex-row-reverse' : 'flex-row'
+      } flex flex-col md:flex-row items-center mb-8 rounded overflow-hidden shadow-lg p-8`}
+    >
+      {isImage ? ImageDispaly : Content}
+    </div>
+  );
+};
+
+export default CompetitionItem;
---
+++ src/components/competition/CompetitionsList.tsx
@@ -0,0 +1,24 @@
+import { JSX } from 'solid-js';
+import CompetitionItem from './CompetitionItem';
+import {
+  Competition,
+} from '../../data/props';
+
+type CompetitionsProps = {
+  competitions: Competition[];
+}
+
+const CompetitionsList = (props: CompetitionsProps): JSX.Element => {
+  const { competitions } = props;
+
+  return (
+    <section>
+      <h2 class="text-3xl font-bold mb-4">Competitions</h2>
+      {competitions.map((competition, index) => (
+        <CompetitionItem competition={competition} index={index} />
+      ))}
+    </section>
+  );
+};
+
+export default CompetitionsList;
