96628ba3ea5fd77659183f8845410290a0382c19
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-04-30 10:47:04 +0900

    Update plan courses & skills

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,7 @@
 import { JSX } from 'solid-js';
 
 const SectionTitle = ({ title }: { title: string }): JSX.Element => (
-  <h1 class="text-2xl md:text-4xl font-bold mb-4 flex justify-center">{title}</h1>
+  <h1 class="text-2xl md:text-4xl font-bold mb-4 flex justify-center items-center text-center">{title}</h1>
 );
 
 export default SectionTitle;
