341b165a082cf24bb44e822aaf1ba9c70d1ea505
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-04-30 11:13:39 +0900

    Organize project

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,33 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val))
-			.optional(),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    tags: z.array(z.string()),
+    language: z.enum(['en', 'th']),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, project };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,21 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  const projects = await getCollection('project');
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: [
+      ...posts.map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      })),
+      ...projects.map((project) => ({
+        ...project.data,
+        link: `/project/${project.slug}/`,
+      })),
+    ],
+  });
 }
