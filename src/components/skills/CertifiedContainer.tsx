e487ef66074734ddad9ece1e3d8d05cdb1b618c7
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-12 01:36:46 +0900

    Calculate expired from expiredDate instead

--- src/components/skills/CertifiedContainer.tsx
+++ src/components/skills/CertifiedContainer.tsx
@@ -23,16 +23,17 @@ const CertifiedContainer = (props: CertifiedContainerProps) => (
         <h3 class="text-lg font-bold mb-2">{props.label}</h3>
         <div class="flex flex-wrap gap-2">
             <For each={props.certifiedList}>
-                {(certified) => (
-                    <a
+                {(certified) => {
+                    const isExpired = new Date(certified.expiredDate) < new Date();
+                    return (<a
                         href={certified.resources[0]?.url}
                         target="_blank"
                         rel="noopener noreferrer"
                         class="bg-primary-light text-text py-2 px-4 rounded-full text-sm hover:bg-primary-dark hover:text-white transition-colors"
                     >
-                        {certified.name}
-                    </a>
-                )}
+                        {certified.name}{isExpired ? ' [EXPIRED]' : ''}
+                    </a>)
+                }}
             </For>
         </div>
     </div>
